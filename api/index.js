ae03d3218e1f08616023fbe51594e482a0cddad9
mazidhussain <mazidhussain71@gmail.com>
2023-03-12 18:40:10 +0530

    latest commit

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const jwtSecret = 'MazidHussainAnsari';
 
 app.use(cors({
     credentials:true,
-    origin:'http://127.0.0.1:5173'
+    origin:process.env.ORIGIN,
 }));
 mongoose.connect(process.env.MONGO_URL);
 
